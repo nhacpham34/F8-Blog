b36f6cf73ccedebb3423bb95a0e8e34afef4c631
PhamNhac <itk40b@gmail.com>
2021-05-08 23:19:18 +0700

    add libaris in forder

---
+++ src/main.js
@@ -0,0 +1,31 @@
+const path = require('path')
+const express = require('express')
+const morgan = require('morgan')
+const handlebars = require('express-handlebars')
+const app = express()
+const port = 3000
+
+// file tĩnh
+app.use(express.static(path.join(__dirname, 'public')))
+//  HTTP logger
+app.use(morgan('combined'));
+
+// Template engine
+app.engine('hbs', handlebars({
+  extname: '.hbs'
+}));
+
+
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname,'resources\\views'))
+
+app.get('/', (req, res) => {
+  res.render('home');
+})
+app.get('/new', (req, res) => {
+  res.render('new');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
